ac9ce3285493260fb8546db5cf4569dee9a4a1b4
MaryJanne_0712 <alunamarianaturossi@gmail.com>
2024-09-25 09:20:02 -0300

    Projeto_Questionario.py

---
+++ Projeto_Questionario.py
@@ -0,0 +1,98 @@
+print (' Bem Vind* ao Questionário sobre Redes!! Escolha uma alternativa e descubra se está correta.')
+
+
+Nome = input ('Digite seu nome:')
+resposta = input ('Clique em qualquer letra para começar!!:')
+
+
+print ('1- Assinale qual alternativa corresponde ao local que usamos o cabo de Rede “T568A” ')
+print (' a) Usado em companhias telefônicas; ')
+print (' b) Usado em ambientes corporativos e domésticos; ')
+print (' c) Usado em ambos os ambientes')
+
+
+resposta  = input ('Digite sua resposta')
+
+
+if resposta == 'a' :
+   print ('!!!resposta certa!!!')
+if resposta == 'b' :
+   print ('_resposta errada_')
+if resposta == 'c' :
+   print ('_reposta errada_')
+
+
+print ('2- Qual a sequência de cores do cabo de Ethernet? ')
+print (' a) Branco+verde, verde, branco+marrom, azul, laranja, branco+azul, marrom, branco+laranja; ')
+print (' b) Branco+verde, verde, branco+azul, laranja, branco+laranja, azul, branco+marrom, marrom;')
+print (' c) Branco+azul, marrom, branco+verde, laranja, branco+marrom,verde, branco+laranja, marrom.')
+
+
+resposta = input (' Digite sua resposta')
+
+
+if resposta == 'b' :
+   print ('!!!resposta certa!!!')
+if resposta == 'a' :
+   print ('_resposta errada_')
+if resposta == 'c' :
+   print ('_reposta errada_')
+
+
+print ('3- Qual a taxa máxima de transferência de um cabo Gigabit Ethernet? ')
+print (' a) 10Mb/s;')
+print (' b) 50Mb/s; ')
+print (' c) 250Mb/s; ')
+print (' d) 1000Mb/s.')
+
+
+resposta = input ('Digite sua resposta')
+
+
+if resposta == 'd' :
+   print ('!!!resposta certa!!!')
+if resposta == 'a' :
+   print ('_resposta errada_')
+if resposta == 'c' :
+   print ('_reposta errada_')
+if resposta == 'b' :
+   print ('_resposta errada_')
+
+
+print ('4- Quais os tipos de Gateway: ')
+print ('a) Gateway Unidirecional e Bidirecional; ')
+print ('b) Gateway Multidirecional e Unidirecional;')
+print ('c)Gateway de Armazenamento e Frete.')
+
+
+resposta = input ('Digite sua resposta')
+
+
+if resposta == 'a' :
+   print ('!!!resposta certa!!!')
+if resposta == 'b' :
+   print ('_resposta errada_')
+if resposta == 'c' :
+   print ('_resposta errada_')
+
+
+print ('5- Que tipo de conexão é “Geograficamente Distribuída”? ')
+print ('a) MANs;')
+print ('b) LANs;')
+print ('c) WANs.')
+
+
+resposta = input ('Digite sua resposta')
+
+
+if resposta == 'a' :
+   print ('_resposta errada_')
+if resposta == 'b' :
+   print ('_resposta errada_')
+if resposta == 'c' :
+   print ('!!!reposta certa!!!')
+
+
+   print ("Obrigado por jogar!!")
+   print ('Espero que tenha se divertido', Nome)
+
